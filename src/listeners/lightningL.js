03645783d3786ec5ca958ba9cca2f5eb20a9b971
Hui Zheng <2249215+hui-zheng@users.noreply.github.com>
2024-01-03 17:21:00 -0800

    final

--- src/listeners/lightningL.js
+++ src/listeners/lightningL.js
@@ -18,7 +18,7 @@ const startLightningListener = async () => {
     await ws.on('message', function message(data) {
     //console.log('Received message from server:', data);
     const messageStr = data.toString('utf-8'); // Convert buffer to string
-    console.log('Received message from server:', messageStr);
+    console.log('Received message from lightning lnbit server:', messageStr);
     // example: 0-1000
     pinNo = messageStr.split('-')[0];
     dispenseFromPayments(pinNo, 'sats')
