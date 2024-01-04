43d8629957941f82a4bcd8de80207ea61603c7ec
Hui Zheng <2249215+hui-zheng@users.noreply.github.com>
2024-01-03 17:18:22 -0800

    2nd draft

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const { startDiscordListener } = require('./src/listeners/discordL');
 const { startPolygonListener } = require('./src/listeners/polygonL');
 const { startMachineChecker } = require('./src/listeners/machineL');
 
+const { startLightningListener } = require('./src/listeners/lightningL');
+
 const main = async () => {
   startDiscordListener();
   startPolygonListener();
+  startLightningListener();
   startMachineChecker();
 };
 main();
--- src/listeners/lightningL.js
+++ src/listeners/lightningL.js
@@ -4,24 +4,33 @@ const { dispenseFromPayments } = require('../machine');
 // WebSocket URL
 const wsUrl = 'wss://send.laisee.org/api/v1/ws/V5UfKc845cdrKrsmMijExW';
 
-// Create a new WebSocket connection
-const ws = new WebSocket(wsUrl);
+const startLightningListener = async () => {
+        
+    // Create a new WebSocket connection
+    const ws = new WebSocket(wsUrl);
 
+    // Event listener for when the connection is open
+    await ws.on('open', function open() {
+    console.log('Connected to the lightning lnbit.');
+    });
 
-// Event listener for when the connection is open
-ws.on('open', function open() {
-  console.log('Connected to the server.');
-});
+    // Event listener for when a message is received from the server
+    await ws.on('message', function message(data) {
+    //console.log('Received message from server:', data);
+    const messageStr = data.toString('utf-8'); // Convert buffer to string
+    console.log('Received message from server:', messageStr);
+    // example: 0-1000
+    pinNo = messageStr.split('-')[0];
+    dispenseFromPayments(pinNo, 'sats')
+    });
 
-// Event listener for when a message is received from the server
-ws.on('message', function message(data) {
-  //console.log('Received message from server:', data);
-  const messageStr = data.toString('utf-8'); // Convert buffer to string
-  console.log('Received message from server:', messageStr);
-  dispenseFromPayments()
-});
+    // Event listener for handling errors
+    await ws.on('error', function error(err) {
+    console.error('WebSocket error:', err);
+    });
 
-// Event listener for handling errors
-ws.on('error', function error(err) {
-  console.error('WebSocket error:', err);
-});
+  };
+  
+  module.exports = {
+    startLightningListener,
+  };
